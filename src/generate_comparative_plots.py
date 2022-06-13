8295b394e0b2efb48aa8a60ee055c232b13e9b5f
Ivan Todorov <ivan.s.t@icloud.com>
2022-06-13 08:33:28 +0200

    Added the results of multiple runs of the quantitative tests and updated the plot-generating scipts to reflect the results

--- src/generate_comparative_plots.py
+++ src/generate_comparative_plots.py
@@ -8,107 +8,128 @@ import matplotlib.pyplot as plt
 x_labels = ["Real", "User", "System"]
 x_axis = np.arange(len(x_labels))
 
-y_with_effects = [17.047, 16.153, 0.792]
-y_without_effects = [17.047, 16.153, 0.792]
+all_with_effects = np.array([[153.79, 145.16, 7.74], [153.37, 145.14, 7.31], [152.81, 144.41, 7.51], [152.92, 144.60, 7.50]])
+all_without_effects = np.array([[134.17, 126.76, 6.50], [136.18, 128.62, 6.61], [135.17, 127.75, 6.47], [135.11, 127.57, 6.64]])
 
+repetitions = 10
 bar_width = 0.35
 
-fig, ax = plt.subplots()
-bars_with_effects = ax.bar(x_axis - bar_width / 2, y_with_effects, bar_width, label="With effects")
-bars_without_effects = ax.bar(x_axis + bar_width / 2, y_without_effects, bar_width, label="Without effects")
-ax.bar_label(bars_with_effects)
-ax.bar_label(bars_without_effects)
-plt.title("Average compilation time of the game (10 compilations)")
-ax.set_xticks(x_axis)
-ax.set_xticklabels(x_labels)
-plt.ylabel("Time (s)")
-ax.legend()
-plt.show()
+for temp_with_effects, temp_without_effects in zip(all_with_effects, all_without_effects):
+    y_with_effects = np.around(temp_with_effects / repetitions, 2)
+    y_without_effects = np.around(temp_without_effects / repetitions, 2)
+    
+    fig, ax = plt.subplots()
+    bars_with_effects = ax.bar(x_axis - bar_width / 2, y_with_effects, bar_width, label="With effects")
+    bars_without_effects = ax.bar(x_axis + bar_width / 2, y_without_effects, bar_width, label="Without effects")
+    ax.bar_label(bars_with_effects)
+    ax.bar_label(bars_without_effects)
+    plt.title("Average compilation time of the game (10 compilations)")
+    ax.set_xticks(x_axis)
+    ax.set_xticklabels(x_labels)
+    plt.ylabel("Time (s)")
+    ax.legend()
+    plt.show()
 
 ## Start time
 
 x_labels = ["Real", "User", "System"]
 x_axis = np.arange(len(x_labels))
 
-y_with_effects = [1.0, 0.7, 0.3]
-y_without_effects = [1.0, 0.7, 0.3]
+all_with_effects = np.array([[0.09, 0.06, 0.02], [0.14, 0.11, 0.04], [0.09, 0.06, 0.03], [0.14, 0.11, 0.04]])
+all_without_effects = np.array([[0.09, 0.06, 0.03], [0.10, 0.07, 0.03], [0.09, 0.07, 0.03], [0.09, 0.06, 0.03]])
 
+repetitions = 100
 bar_width = 0.35
 
-fig, ax = plt.subplots()
-bars_with_effects = ax.bar(x_axis - bar_width / 2, y_with_effects, bar_width, label="With effects")
-bars_without_effects = ax.bar(x_axis + bar_width / 2, y_without_effects, bar_width, label="Without effects")
-ax.bar_label(bars_with_effects)
-ax.bar_label(bars_without_effects)
-plt.title("Average start time of the game (100 simulations)")
-ax.set_xticks(x_axis)
-ax.set_xticklabels(x_labels)
-plt.ylabel("Time (ms)")
-ax.legend()
-plt.show()
+for temp_with_effects, temp_without_effects in zip(all_with_effects, all_without_effects):
+    y_with_effects = np.around(temp_with_effects / repetitions * 1_000, 2)
+    y_without_effects = np.around(temp_without_effects / repetitions * 1_000, 2)
+        
+    fig, ax = plt.subplots()
+    bars_with_effects = ax.bar(x_axis - bar_width / 2, y_with_effects, bar_width, label="With effects")
+    bars_without_effects = ax.bar(x_axis + bar_width / 2, y_without_effects, bar_width, label="Without effects")
+    ax.bar_label(bars_with_effects)
+    ax.bar_label(bars_without_effects)
+    plt.title("Average start time of the game (100 simulations)")
+    ax.set_xticks(x_axis)
+    ax.set_xticklabels(x_labels)
+    plt.ylabel("Time (ms)")
+    ax.legend()
+    plt.show()
 
 ## One player's turn (shortest game)
 
 x_labels = ["Real", "User", "System"]
 x_axis = np.arange(len(x_labels))
 
-y_with_effects = [135.294, 100.0, 47.059]
-y_without_effects = [135.294, 100.0, 47.059]
+all_with_effects = np.array([[0.12, 0.10, 0.03], [0.22, 0.17, 0.06], [0.10, 0.08, 0.02], [0.24, 0.19, 0.07]])
+all_without_effects = np.array([[0.17, 0.14, 0.04], [0.24, 0.19, 0.06], [0.23, 0.16, 0.08], [0.10, 0.08, 0.02]])
 
+repetitions = 100
 bar_width = 0.35
 
-fig, ax = plt.subplots()
-bars_with_effects = ax.bar(x_axis - bar_width / 2, y_with_effects, bar_width, label="With effects")
-bars_without_effects = ax.bar(x_axis + bar_width / 2, y_without_effects, bar_width, label="Without effects")
-ax.bar_label(bars_with_effects)
-ax.bar_label(bars_without_effects)
-plt.title("Average time for executing a player's turn in the\nshortest decisive world championship game\n(100 simulations)")
-ax.set_xticks(x_axis)
-ax.set_xticklabels(x_labels)
-plt.ylabel(r"Time ($\mu$s)")
-ax.legend()
-plt.show()
+for temp_with_effects, temp_without_effects in zip(all_with_effects, all_without_effects):
+    y_with_effects = np.around(temp_with_effects / repetitions / 17 * 1_000_000, 2)
+    y_without_effects = np.around(temp_without_effects / repetitions / 17 * 1_000_000, 2)
+
+    fig, ax = plt.subplots()
+    bars_with_effects = ax.bar(x_axis - bar_width / 2, y_with_effects, bar_width, label="With effects")
+    bars_without_effects = ax.bar(x_axis + bar_width / 2, y_without_effects, bar_width, label="Without effects")
+    ax.bar_label(bars_with_effects)
+    ax.bar_label(bars_without_effects)
+    plt.title("Average time for executing a player's turn in the\nshortest decisive world championship game\n(100 simulations)")
+    ax.set_xticks(x_axis)
+    ax.set_xticklabels(x_labels)
+    plt.ylabel(r"Time ($\mu$s)")
+    ax.legend()
+    plt.show()
 
 ## One player's turn (longest game)
 
-x_label = ["Real", "User", "System"]
-x_axis = np.arange(len(x_label))
+x_labels = ["Real", "User", "System"]
+x_axis = np.arange(len(x_labels))
 
-y_with_effects = [17.647, 13.235, 5.147]
-y_without_effects = [17.647, 13.235, 5.147]
+all_with_effects = np.array([[0.24, 0.18, 0.08], [0.24, 0.17, 0.09], [0.22, 0.16, 0.08], [0.25, 0.18, 0.08]])
+all_without_effects = np.array([[0.25, 0.19, 0.07], [0.24, 0.19, 0.07], [0.25, 0.18, 0.08], [0.24, 0.18, 0.07]])
 
+repetitions = 100
 bar_width = 0.35
 
-fig, ax = plt.subplots()
-bars_with_effects = ax.bar(x_axis - bar_width / 2, y_with_effects, bar_width, label="With effects")
-bars_without_effects = ax.bar(x_axis + bar_width / 2, y_without_effects, bar_width, label="Without effects")
-ax.bar_label(bars_with_effects)
-ax.bar_label(bars_without_effects)
-plt.title("Average time for executing a player's turn in the\nlongest decisive world championship game\n(100 simulations)")
-ax.set_xticks(x_axis)
-ax.set_xticklabels(x_labels)
-plt.ylabel(r"Time ($\mu$s)")
-ax.legend()
-plt.show()
+for temp_with_effects, temp_without_effects in zip(all_with_effects, all_without_effects):
+    y_with_effects = np.around(temp_with_effects / repetitions / 136 * 1_000_000, 2)
+    y_without_effects = np.around(temp_without_effects / repetitions / 136 * 1_000_000, 2)
+    
+    fig, ax = plt.subplots()
+    bars_with_effects = ax.bar(x_axis - bar_width / 2, y_with_effects, bar_width, label="With effects")
+    bars_without_effects = ax.bar(x_axis + bar_width / 2, y_without_effects, bar_width, label="Without effects")
+    ax.bar_label(bars_with_effects)
+    ax.bar_label(bars_without_effects)
+    plt.title("Average time for executing a player's turn in the\nlongest decisive world championship game\n(100 simulations)")
+    ax.set_xticks(x_axis)
+    ax.set_xticklabels(x_labels)
+    plt.ylabel(r"Time ($\mu$s)")
+    ax.legend()
+    plt.show()
 
 ## Peak memory usage
 
 x_labels = ["Shortest game", "Longest game"]
 x_axis = np.arange(len(x_labels))
 
-y_with_effects = [3424, 3300]
-y_without_effects = [3424, 3300]
+all_with_effects = np.array([[3404, 3356], [3420, 3380], [3460, 3300], [3428, 3432]])
+all_without_effects = np.array([[3392, 3400], [3396, 3344], [3384, 3304], [3336, 3432]])
 
 bar_width = 0.35
 
-fig, ax = plt.subplots()
-bars_with_effects = ax.bar(x_axis - bar_width / 2, y_with_effects, bar_width, label="With effects")
-bars_without_effects = ax.bar(x_axis + bar_width / 2, y_without_effects, bar_width, label="Without effects")
-ax.bar_label(bars_with_effects)
-ax.bar_label(bars_without_effects)
-plt.title("Peak memory usage during 100 simulations of a game")
-ax.set_xticks(x_axis)
-ax.set_xticklabels(x_labels)
-plt.ylabel("Memory usage (kilobytes)")
-ax.legend()
-plt.show()
+for y_with_effects, y_without_effects in zip(all_with_effects, all_without_effects):
+    fig, ax = plt.subplots()
+    bars_with_effects = ax.bar(x_axis - bar_width / 2, y_with_effects, bar_width, label="With effects")
+    bars_without_effects = ax.bar(x_axis + bar_width / 2, y_without_effects, bar_width, label="Without effects")
+    ax.bar_label(bars_with_effects)
+    ax.bar_label(bars_without_effects)
+    plt.title("Peak memory usage during 100 simulations of a game")
+    ax.set_xticks(x_axis)
+    ax.set_xticklabels(x_labels)
+    plt.ylabel("Memory usage (kilobytes)")
+    ax.legend(loc='lower left')
+    plt.show()
--- src/without-effects/scripts/generate_plots.py
+++ src/without-effects/scripts/generate_plots.py
@@ -1,58 +1,107 @@
 #!/usr/bin/python3.8
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 ## Compilation time
 
 x_axis = ["Real", "User", "System"]
-y_axis = [17.047, 16.153, 0.792]
+y_all_compilation = np.array([[134.17, 126.76, 6.50], [136.18, 128.62, 6.61], [135.17, 127.75, 6.47], [135.11, 127.57, 6.64]])
 
-fig, ax = plt.subplots()
-bars = ax.bar(x_axis, y_axis)
-ax.bar_label(bars)
-plt.title("Average compilation time of the game (10 compilations)")
-plt.ylabel("Time (s)")
+repetitions = 10
+
+for y_temp in y_all_compilation:
+    y_axis = np.around(y_temp / repetitions, 2)
+
+    fig, ax = plt.subplots()
+    bars = ax.bar(x_axis, y_axis)
+    ax.bar_label(bars)
+    plt.title("Average compilation time of the game (10 compilations)")
+    plt.ylabel("Time (s)")
+    plt.show()
 
 ## Start time
 
 x_axis = ["Real", "User", "System"]
-y_axis = [1.0, 0.7, 0.3]
+y_all_start = np.array([[0.09, 0.06, 0.03], [0.10, 0.07, 0.03], [0.09, 0.07, 0.03], [0.09, 0.06, 0.03]])
 
-fig, ax = plt.subplots()
-bars = ax.bar(x_axis, y_axis)
-ax.bar_label(bars)
-plt.title("Average start time of the game (100 simulations)")
-plt.ylabel("Time (ms)")
+repetitions = 100
+
+for y_temp in y_all_start:
+    y_axis = np.around(y_temp / repetitions * 1_000, 2)
+
+    fig, ax = plt.subplots()
+    bars = ax.bar(x_axis, y_axis)
+    ax.bar_label(bars)
+    plt.title("Average start time of the game (100 simulations)")
+    plt.ylabel("Time (ms)")
 
 ## One player's turn (shortest game)
 
 x_axis = ["Real", "User", "System"]
-y_axis = [135.294, 100.0, 47.059]
+y_all_short = np.array([[0.17, 0.14, 0.04], [0.24, 0.19, 0.06], [0.23, 0.16, 0.08], [0.10, 0.08, 0.02]])
 
-fig, ax = plt.subplots()
-bars = ax.bar(x_axis, y_axis)
-ax.bar_label(bars)
-plt.title("Average time for executing a player's turn in the\nshortest decisive world championship game\n(100 simulations)")
-plt.ylabel(r"Time ($\mu$s)")
+repetitions = 100
+
+for y_temp in y_all_short:
+    y_axis = np.around(y_temp / repetitions / 17 * 1_000_000, 2)
+
+    fig, ax = plt.subplots()
+    bars = ax.bar(x_axis, y_axis)
+    ax.bar_label(bars)
+    plt.title("Average time for executing a player's turn in the\nshortest decisive world championship game\n(100 simulations)")
+    plt.ylabel(r"Time ($\mu$s)")
 
 ## One player's turn (longest game)
 
 x_axis = ["Real", "User", "System"]
-y_axis = [17.647, 13.235, 5.147]
+y_all_long = np.array([[0.25, 0.19, 0.07], [0.24, 0.19, 0.07], [0.25, 0.18, 0.08], [0.24, 0.18, 0.07]])
 
-fig, ax = plt.subplots()
-bars = ax.bar(x_axis, y_axis)
-ax.bar_label(bars)
-plt.title("Average time for executing a player's turn in the\nlongest decisive world championship game\n(100 simulations)")
-plt.ylabel(r"Time ($\mu$s)")
+repetitions = 100
+
+for y_temp in y_all_long:
+    y_axis = np.around(y_temp / repetitions / 136 * 1_000_000, 2)
+
+    fig, ax = plt.subplots()
+    bars = ax.bar(x_axis, y_axis)
+    ax.bar_label(bars)
+    plt.title("Average time for executing a player's turn in the\nlongest decisive world championship game\n(100 simulations)")
+    plt.ylabel(r"Time ($\mu$s)")
+
+## One player's turn comparison
+
+x_labels = ["Real", "User", "System"]
+x_axis = np.arange(len(x_labels))
+
+repetitions = 100
+bar_width = 0.35
+
+for y_temp_short, y_temp_long in zip(y_all_short, y_all_long):
+    y_short = np.around(y_temp_short / repetitions / 17 * 1_000_000, 2)
+    y_long = np.around(y_temp_long / repetitions / 136 * 1_000_000, 2)
+    
+    fig, ax = plt.subplots()
+    bars_short = ax.bar(x_axis - bar_width / 2, y_short, bar_width, label="Shortest game")
+    bars_long = ax.bar(x_axis + bar_width / 2, y_long, bar_width, label="Longest game")
+    ax.bar_label(bars_short)
+    ax.bar_label(bars_long)
+    plt.title("Comparison of average time for executing a player's turn\n(100 simulations)")
+    ax.set_xticks(x_axis)
+    ax.set_xticklabels(x_labels)
+    plt.ylabel(r"Time ($\mu$s)")
+    ax.legend()
+    plt.show()
 
 ## Peak memory usage
 
 x_axis = ["Shortest game", "Longest game"]
-y_axis = [3424, 3300]
+y_all_memory = np.array([[3392, 3400], [3396, 3344], [3384, 3304], [3336, 3432]])
 
-fig, ax = plt.subplots()
-bars = ax.bar(x_axis, y_axis)
-ax.bar_label(bars)
-plt.title("Peak memory usage during 100 simulations of a game")
-plt.ylabel("Memory usage (kilobytes)")
+repetitions = 100
+
+for y_axis in y_all_memory:
+    fig, ax = plt.subplots()
+    bars = ax.bar(x_axis, y_axis)
+    ax.bar_label(bars)
+    plt.title("Peak memory usage during 100 simulations of a game")
+    plt.ylabel("Memory usage (kilobytes)")
